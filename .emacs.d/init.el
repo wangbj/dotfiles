65bcbfc84bac05ca0e846173ec94139eb43b0e07
Baojun Wang <wangbj@gmail.com>
2019-06-28 15:30:45 -0400

    update init.el to favor LSP

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,15 +8,9 @@
 (eval-and-compile
   (setq load-path
         (append (delete-dups load-path)
-                '("~/.emacs.d/lisp"))))
+                '("~/.emacs.d/elpa"))))
 
 (require 'package)
-
-;; keep the installed packages in .emacs.d
-(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
-
-; list the packages you want
-(setq package-list '(intero racer cargo flycheck lsp-rust magit magithub irony irony-eldoc flycheck-irony flycheck-rust company-irony groovy-mode company auto-complete iedit utop tuareg merlin merlin-eldoc ocp-indent))
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
@@ -27,6 +21,12 @@
 ; fetch the list of packages available
 (unless package-archive-contents
   (package-refresh-contents))
+
+;; keep the installed packages in .emacs.d
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+
+; list the packages you want
+(setq package-list '(intero cargo flycheck lsp-mode lsp-treemacs helm-lsp toml-mode dap-mode lsp-ui rustic magit magithub irony irony-eldoc flycheck-irony flycheck-rust company-lsp company-irony groovy-mode company auto-complete iedit utop tuareg merlin merlin-eldoc ocp-indent protobuf-mode yasnippet yasnippet-snippets))
 
 ; install the missing packages
 (dolist (package package-list)
@@ -49,6 +49,24 @@
 (set-keyboard-coding-system 'utf-8)
 (set-language-environment 'utf-8)
 
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(require 'lsp)
+(require 'lsp-clients)
+(require 'lsp-ui)
+(require 'toml-mode)
+(add-hook 'lsp-mode-hook   #'lsp-ui-mode)
+
+(require 'company-lsp)
+(push 'company-lsp company-backends)
+
+(when (>= emacs-major-version 26)
+  (require 'rustic)
+  (require 'rustic-compile)
+  (setq rustic-lsp-server 'rls)
+  (add-hook 'rustic-mode-hook  #'lsp))
+
 ;; set c-basic-offset
 ; (setq-default c-basic-offset 4)
 
@@ -65,32 +83,30 @@
 
 (add-hook 'haskell-mode-hook 'intero-mode)
 
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'rust-mode-hook 'cargo-minor-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook #'company-mode)
-(add-hook 'racer-mode-hook #'cargo-minor-mode)
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-(setq company-tooltip-align-annotations t)
+;; (add-hook 'rust-mode-hook #'racer-mode)
+;; (add-hook 'rust-mode-hook 'cargo-minor-mode)
+;; (add-hook 'racer-mode-hook #'eldoc-mode)
+;; (add-hook 'racer-mode-hook #'company-mode)
+;; (add-hook 'racer-mode-hook #'cargo-minor-mode)
+;; (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
+;; (add-hook 'rust-mode-hook #'lsp-rust)
+;; (add-hook 'rust-mode-hook #'flycheck-mode)
 
-;; (with-eval-after-load 'lsp-mode
-;;  (setq lsp-rust-rls-command '("rustup" "run" "nightly" "rls"))
-;;  (require 'lsp-rust))
-
-;;(add-hook 'rust-mode-hook #'lsp-rust-enable)
-;;(add-hook 'rust-mode-hook #'flycheck-mode)
+(add-hook 'c++-mode-hook #'lsp)
+(add-hook 'c-mode-hook #'lsp)
+(add-hook 'objc-mode-hook #'lsp)
 
 ; irony mode
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-(add-hook 'irony-mode-hook #'irony-eldoc)
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
+;; (add-hook 'c++-mode-hook 'irony-mode)
+;; (add-hook 'c-mode-hook 'irony-mode)
+;; (add-hook 'objc-mode-hook 'irony-mode)
+;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+;; (add-hook 'irony-mode-hook #'irony-eldoc)
+;; (eval-after-load 'flycheck
+;;  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+;;(eval-after-load 'company
+;;  '(add-to-list 'company-backends 'company-irony))
 
 (if (file-exists-p "/usr/local/share/emacs/site-lisp/cask/cask.el")
     ( progn
