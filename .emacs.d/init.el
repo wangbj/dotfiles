9e6049ecf3eef00e6a345fc83c8c7aa7e3d33977
Baojun Wang <wangbj@gmail.com>
2026-03-19 22:07:01 -0400

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,7 +16,7 @@
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
 
 ; list the packages you want
-(setq package-list '(company flycheck magit dap-mode which-key rust-mode lsp-mode lsp-haskell use-package))
+(setq package-list '(auctex company flycheck lsp-ui magit magithub dap-mode which-key lsp-mode lsp-haskell rustic use-package))
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
@@ -60,10 +60,7 @@
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 (use-package lsp-mode
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  :hook ((rust-mode . lsp)
+  :hook ((rustic-mode . lsp)
          (python-mode . lsp)
          (haskell-mode . lsp)
          (haskell-literate-mode . lsp)
@@ -71,25 +68,18 @@
          (c++-mode . lsp)
          (objc-mode . lsp)
          (go-mode . lsp)
-         ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp
-  ;; :config
-  ;; (setq lsp-idle-delay 0.500)
+  :config
+  (setq lsp-idle-delay 0.500)
   )
 
-;; rust-ts-mode, requires emacs 29.x
-;; (use-package rust-mode
-;;  :init
-;;  (setq rust-mode-treesitter-derive t))
-
-(add-hook 'rust-mode-hook #'lsp)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
-;; (use-package lsp-ui :commands lsp-ui-mode)
-;; (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-;; (use-package dap-mode)
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+(use-package dap-mode)
 
 (use-package which-key
   :config
@@ -99,9 +89,31 @@
   (setq lsp-diagnostics-modeline-scope :project)
   (add-hook 'lsp-managed-mode-hook 'lsp-diagnostics-modeline-mode))
 
-(setq lsp-rust-server 'rust-analyzer)
-;; (setq lsp-rust-analyzer-server-command '("~/.cargo/bin/rust-analyzer"))
+;; Rustic, LSP
+(require 'rustic)
 
+;; (setq rustic-lsp-client nil)
+(setq rustic-lsp-server 'rust-analyzer)
+(setq lsp-rust-analyzer-server-command '("~/.cargo/bin/rust-analyzer"))
+(setq rustic-format-on-save t)
+;; (push 'rustic-clippy flycheck-checkers)
+;; (remove-hook 'rustic-mode-hook 'flycheck-mode)
+
+;; Set up AUCTeX and connect it to lsp-mode
+(use-package tex
+  :ensure auctex
+  :hook ((LaTeX-mode . lsp-deferred)
+         (LaTeX-mode . turn-on-reftex))
+  :config
+  ;; Recommended AUCTeX settings for parsing documents
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  ;; Set XeLaTeX as the default compilation engine
+  (setq-default TeX-engine 'xetex)
+  ;; Force AUCTeX to compile straight to PDF
+  (setq-default TeX-PDF-mode t)
+  ;; Ask for the master file when creating a new document
+  (setq-default TeX-master nil))
 
 (provide 'init)
 (custom-set-variables
@@ -109,24 +121,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(lsp-haskell which-key dap-mode rust-mode magit lsp-mode flycheck company)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda-mode locate")))
-
-;; auto-load agda-mode for .agda and .lagda.md
-(setq auto-mode-alist
-   (append
-     '(("\\.agda\\'" . agda2-mode)
-       ("\\.lagda.md\\'" . agda2-mode))
-     auto-mode-alist))
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
